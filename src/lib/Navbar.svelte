f67d03f1b0f4fed8367dce83cc053595909859b5
Victor Pudo <cpt.victor@hotmail.com>
2023-07-02 12:38:18 -0300

    feat: add new login route and initial page

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -10,6 +10,7 @@
         <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
       </a>
     </div>
+
     <div class="flex lg:hidden">
       <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5">
         <span class="sr-only">Open main menu</span>
@@ -18,25 +19,30 @@
         </svg>
       </button>
     </div>
+
     <div class="hidden lg:flex lg:gap-x-12">
       <a href="/" class="text-sm font-semibold leading-6">Home</a>
       <a href="atum" class="text-sm font-semibold leading-6">Atum</a>
       <a href="sazon" class="text-sm font-semibold leading-6">Sazon</a>
     </div>
+
     <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="#" class="text-sm font-semibold leading-6">Log in <span aria-hidden="true">&rarr;</span></a>
+      <a href="login" class="text-sm font-semibold leading-6">Log in <span aria-hidden="true">&rarr;</span></a>
     </div>
   </nav>
+
   <!-- Mobile menu, show/hide based on menu open state. -->
   <div class="lg:hidden" role="dialog" aria-modal="true">
     <!-- Background backdrop, show/hide based on slide-over state. -->
     <div class="fixed inset-0 z-50"></div>
+
     <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div class="flex items-center justify-between">
-        <a href="#" class="-m-1.5 p-1.5">
+        <a href="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
           <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
         </a>
+
         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Close menu</span>
           <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -44,6 +50,7 @@
           </svg>
         </button>
       </div>
+
       <div class="mt-6 flow-root">
         <div class="-my-6 divide-y divide-gray-500/10">
           <div class="space-y-2 py-6">
@@ -51,8 +58,9 @@
             <a href="atum" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-700">Atum</a>
             <a href="sazon" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-700">Sazon</a>
           </div>
+          
           <div class="py-6">
-            <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-gray-700">Log in</a>
+            <a href="login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-gray-700">Log in</a>
           </div>
         </div>
       </div>
